970b01762f0be2b7874b34bfcc5f913789053534
Your Name <you@example.com>
2025-03-12 20:08:48 +0800

    Started creating navbar

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-document.querySelector(".enter-site").addEventListener("click", enterSite);
+//Button to enter site
 
 function enterSite() {
   window.location.href = "new-page.html";
 }
+
+const siteButton = document
+  .querySelector(".enter-site")
+  .addEventListener("click", () => {
+    enterSite();
+  });
+
+//Using named functions can clean up your code considerably, and is a really good idea if the function is something that you are going to want to do in multiple places.
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+//A callback is a function that is passed into another function as an argument.
